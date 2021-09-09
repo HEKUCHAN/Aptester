bf7a77bebbe963efb898314d0d413c070a49eaee
HEKUCHAN <Heitorhirose@gmail.com>
2021-09-10 00:03:43 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+__author__ = 'Heitor Hirose'
+
+setup(
+    name='Auto-Python-Tester',
+    version='0.0.1',
+    description='Auto Tester for Competitive programming',
+    author='Heitor Hirose',
+    author_email='Heitorhirose@gmail.com',
+    url='https://github.com/HEKUCHAN/Auto-Python-Tester',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Japanese',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    keywords=['Image Registration'],
+    license='MIT License',
+    install_requires=[
+    ],
+)
